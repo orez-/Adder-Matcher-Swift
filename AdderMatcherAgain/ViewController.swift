8c09c46c6676235683d182e46b0043f05b931b4e
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2015-09-03 04:05:21 -0400

    Working game (!)
    Not super flashy, but (more or less) totally functional.

--- AdderMatcherAgain/ViewController.swift
+++ AdderMatcherAgain/ViewController.swift
@@ -12,23 +12,34 @@ let color_map:[(background:UIColor, text:UIColor)] = [
     (background:UIColor.redColor(), text:UIColor.whiteColor()),
     (background:UIColor.greenColor(), text:UIColor.blackColor()),
     (background:UIColor.yellowColor(), text:UIColor.blackColor()),
-    (background:UIColor.blueColor(), text:UIColor.blackColor()),
-    (background:UIColor.purpleColor(), text:UIColor.blackColor()),
+    (background:UIColor.blueColor(), text:UIColor.whiteColor()),
+    (background:UIColor.magentaColor(), text:UIColor.whiteColor()),
     (background:UIColor.cyanColor(), text:UIColor.blackColor()),
-    (background:UIColor.whiteColor(), text:UIColor.blackColor())
+    (background:UIColor.grayColor(), text:UIColor.blackColor())
 ]
+
+func delay(delay:Double, closure:()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
+}
 
 
 class ViewController: UIViewController {
-//    @IBOutlet var num:UIButton!
-//    @IBOutlet var buttons:[UIButton]!
-//        var labels:[UILabel]()
-    var game_state:GameState!
+    var game_state:Game!
+    @IBOutlet var board_area:UIView!
+    @IBOutlet var score_box:UITextField!
+    @IBOutlet var health_boxes:[UIView]!
+    private var buttons:[UIButton] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.game_state = GameState()
+        self.game_state = Game()
 
+        updateHealthDisplay()
         for (r, row) in enumerate(self.game_state.board) {
             for (c, elem) in enumerate(row) {
                 addButton(elem, row:r, col:c)
@@ -43,20 +54,64 @@ class ViewController: UIViewController {
 
     private func addButton(value:UInt32, row:Int, col:Int) {
         let button = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-        button.frame = CGRectMake(CGFloat(col * 60) + 50, CGFloat(row * 60) + 80, 50, 50)
-        let button_colors = color_map[(Int(value) - 1) % color_map.count]
+        button.frame = CGRectMake(CGFloat(col * 60) + 10, CGFloat(row * 60) + 10, 50, 50)
+        button.tag = row * SIZE + col
+        button.titleLabel!.font = UIFont(name: "Helvetica-Bold", size: 20)
+        button.addTarget(self, action: "advanceTile:", forControlEvents: UIControlEvents.TouchUpInside)
+        updateButton(button, value:value)
+
+        self.buttons.append(button)
+
+        self.board_area.addSubview(button)
+    }
+
+    private func updateButton(button:UIButton, value:UInt32) {
+        let button_colors:(background:UIColor, text:UIColor)
+        if value == 0 {
+            button_colors = (UIColor.whiteColor(), UIColor.whiteColor())
+        }
+        else {
+            button_colors = color_map[(Int(value) - 1) % color_map.count]
+        }
         button.backgroundColor = button_colors.background
         button.setTitle("\(value)", forState: UIControlState.Normal)
-        button.titleLabel!.font = UIFont(name: "Helvetica-Bold", size: 20)
         button.setTitleColor(button_colors.text, forState: UIControlState.Normal)
-        button.tag = row * SIZE + col
-        button.addTarget(self, action: "advanceTile:", forControlEvents: UIControlEvents.TouchUpInside)
+    }
 
-        self.view.addSubview(button)
+    private func updateHealthDisplay() {
+        let healthColor:UIColor
+        switch(game_state.health) {
+        case 1: healthColor = UIColor.redColor()
+        case 5: healthColor = UIColor.greenColor()
+        default: healthColor = UIColor.yellowColor()
+        }
+
+        for (i, box) in enumerate(health_boxes) {
+            if i < game_state.health {
+                box.backgroundColor = healthColor
+            }
+            else {
+                box.backgroundColor = UIColor(white: 1, alpha: 0)
+            }
+        }
+    }
+
+    private func dispatch_acting() {
+        self.score_box.text = "Score: \(self.game_state.score)"
+        updateHealthDisplay()
+        for button in buttons {
+            let coord2 = Coord(row: button.tag / SIZE, col: button.tag % SIZE)
+            updateButton(button, value: self.game_state.board[coord2.row][coord2.col])
+        }
+        if game_state.act() {
+            delay(0.5, dispatch_acting)
+        }
     }
 
     @IBAction func advanceTile(sender:UIButton) {
         let coord = Coord(row: sender.tag / SIZE, col: sender.tag % SIZE)
-        println(coord)
+        if self.game_state.advance_tile(coord) {
+            dispatch_acting()
+        }
     }
 }
--- GameState.swift
+++ GameState.swift
@@ -32,28 +32,77 @@ func ==(lhs: Coord, rhs: Coord) -> Bool {
 }
 
 
-func in_bounds(coord: Coord) -> Bool {
+private func in_bounds(coord: Coord) -> Bool {
     return 0 <= coord.row && coord.row < SIZE && 0 <= coord.col && coord.col < SIZE
 }
 
 
-class GameState {
-    var board:[[UInt32]]
-    var _highest:UInt32
+enum GameState {
+    case awaiting_action
+    case falling
+    case check_collapse
+    case dead
+}
+
+
+class Game {
+    var board:[[UInt32]]!
+    private var highest:UInt32
     var score:UInt32
-    
+    var state:GameState
+    var health:Int
+
     init() {
-        self.board = Array(map(1...SIZE) {_ in Array(map(1...SIZE) {_ in arc4random_uniform(START_MAX_VALUE) + 1})})
-        self._highest = START_MAX_VALUE
+        self.highest = START_MAX_VALUE
         self.score = 0
-        
-//        self._collapse_all()
+        self.health = MAX_HEALTH
+        self.state = .check_collapse
+        self.board = Array(map(1...SIZE) {_ in Array(map(1...SIZE) {_ in self.random_drop()})})
+
+        while self.act() {}
         
         self.score = 0
     }
+
+    func act() -> Bool {
+        switch self.state {
+        case .falling:
+            fall()
+        case .check_collapse:
+            if !collapse_one() {
+                state = .awaiting_action
+            }
+        case .awaiting_action:
+            return false
+        case .dead:
+            return false
+        }
+        return true
+    }
     
-    func advance_tile(c:Coord) {
-        println(c.row, c.col)
+    func advance_tile(c:Coord) -> Bool {
+        switch self.state {
+        case .awaiting_action: break
+        default: return false
+        }
+        self.board[c.row][c.col] += 1
+        let collapse_set = self._get_matches_at(c)
+        self.health -= 1
+        if collapse_set.count < MINIMUM_COLLAPSE {
+            self.highest = max(self.highest, self.board[c.row][c.col])
+            if self.health == 0 {
+                state = .dead
+                score += board.reduce(0, combine: {(value, row) in row.reduce(value, combine: +)}) * 10
+            }
+            return true
+        }
+
+        collapse(collapse_set, coord: c)
+        return true
+    }
+
+    private func random_drop() -> UInt32 {
+        return arc4random_uniform(highest - 1) + 1
     }
     
     func _neighbors(c:Coord) -> [(Coord, UInt32)] {
@@ -64,23 +113,21 @@ class GameState {
             Coord(row:c.row, col:c.col + 1),
             ], in_bounds).map({(c:Coord) -> (Coord, UInt32) in (c, self.board[c.row][c.col])})
     }
-    
-    func _collapse_all() {
-        while true {
-            let matches = self._find_matches()
-            if matches == nil {break}
-            let (r_c, collapse_set) = matches!
-            self._collapse(collapse_set, coord:r_c)
-        }
+
+    private func collapse_one() -> Bool {
+        let matches = self._find_matches()
+        if matches == nil {return false}
+        let (r_c, collapse_set) = matches!
+        self.collapse(collapse_set, coord:r_c)
+        return true
     }
+
+
+    /// Find the first contiguous set of tiles whose values match in
+    /// a sizeable group (as determined by MINIMUM_COLLAPSE) and return
+    /// the coordinate to which they should collapse and their set.
     
-    /*
-    Find the first contiguous set of tiles whose values match in
-    a sizeable group (as determined by MINIMUM_COLLAPSE) and return
-    the coordinate to which they should collapse and their set.
-    
-    Search order is reading order: left to right, top to bottom.
-    */
+    /// Search order is reading order: left to right, top to bottom.
     func _find_matches() -> (Coord, Set<Coord>)? {
         var seen_set:Set<Coord> = Set()
         for (r, row) in enumerate(self.board) {
@@ -94,11 +141,9 @@ class GameState {
         }
         return nil
     }
-    
-    /*
-    Get the contiguous set of tiles whose values match those at the
-    given coordinate.
-    */
+
+    /// Get the contiguous set of tiles whose values match those at the
+    /// given coordinate.
     func _get_matches_at(coord:Coord) -> Set<Coord> {
         let num = self.board[coord.row][coord.col]
         var collapse_set:Set<Coord> = Set()
@@ -117,12 +162,53 @@ class GameState {
         return collapse_set
     }
     
-    func _collapse(collapse_set:Set<Coord>, coord:Coord) -> [GameState] {
-        var consequences:[GameState] = []
-        
-        // let value = self.board[][]
-        
-        return consequences
+    private func collapse(collapse_set:Set<Coord>, coord:Coord) {
+        let value = self.board[coord.row][coord.col]
+        self.score += value * UInt32(collapse_set.count * 10)
+        self.health = min(MAX_HEALTH, health + 1)
+
+        for c in collapse_set {
+            if c == coord {
+                self.board[c.row][c.col] += 1
+                self.highest = max(self.highest, self.board[c.row][c.col])
+            }
+            else {
+                self.board[c.row][c.col] = 0
+            }
+        }
+        self.state = .falling
+    }
+
+    private func fall() {
+        // Settle all floating pieces
+        for column in 0..<SIZE {
+            var built_row = SIZE - 1
+            var floating_row = SIZE - 1
+
+            while floating_row >= 0 {
+                if board[built_row][column] != 0 {
+                    built_row -= 1
+                    floating_row = min(floating_row, built_row)
+                }
+                else if board[floating_row][column] == 0 {
+                    floating_row -= 1
+                }
+                else {
+                    board[built_row][column] = board[floating_row][column]
+                    board[floating_row][column] = 0
+                }
+            }
+        }
+
+        // Backfill empty spots
+        for (r, row) in enumerate(board) {
+            for (c, elem) in enumerate(row) {
+                if elem == 0 {
+                    board[r][c] = random_drop()
+                }
+            }
+        }
+        state = .check_collapse
     }
 }
 
